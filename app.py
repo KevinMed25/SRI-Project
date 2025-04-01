0c223218226c261b5ec4fe42f31c4182da3379de
Kevin Medina <kevinmedina2509Jr@gmail.com>
2025-04-01 11:18:50 -0600

    fix: view consumes the api

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
 import csv
 
 app = Flask(__name__)
+CORS(app, origins=["http://127.0.0.1:5500"])
 
 @app.route("/")
 def index():
