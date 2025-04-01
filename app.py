b14902844ad7e6552640b623bc9e364de2f7c86a
Alvaro Xool <alvaroxool14@gmail.com>
2025-04-01 10:21:28 -0600

    feat: get movies and users from an api

--- app.py
+++ app.py
@@ -7,5 +7,34 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/api/users", methods=['GET'])
+def get_users(): 
+    users_list = []
+
+    with open('./data/users.csv', mode='r', encoding='utf-8') as csv_file :
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader: 
+            for name in row: 
+                if name.strip(): 
+                    users_list.append({"name": name.strip()})
+
+    return jsonify({"users": users_list})
+    
+
+@app.route('/api/movies', methods=['GET'])
+def get_movies():
+    movies_list = []
+    with open('./data/movies.csv', mode='r', encoding='utf-8') as csv_file :
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader: 
+            movies_list.append({
+                "name": row["name"],
+                "description": row["description"],
+                "category": row["category"]
+            })
+
+    return jsonify({"movies": movies_list})
+
 if __name__ == "__main__":
     app.run(debug=True)
